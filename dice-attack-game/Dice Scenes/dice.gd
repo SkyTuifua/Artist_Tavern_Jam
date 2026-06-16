087aac11d765f70ad069f779324b6d0c8c7bcbd3
SkyTuifua <lt49swegger@outlook.com>
2026-06-16 16:30:50 -0400

    Roll will tell you what you got

--- dice-attack-game/Dice Scenes/dice.gd
+++ dice-attack-game/Dice Scenes/dice.gd
@@ -1,6 +1,8 @@
 extends RigidBody3D
 class_name Dice
 @export var sides : Array[Node3D]
+var current_side : Side
+var dice_moving : bool = false
 signal stopped()
 #func _physics_process(delta: float) -> void:
 	#for i in sides:
@@ -8,15 +10,18 @@ signal stopped()
 
 
 func _on_sleeping_state_changed() -> void:
-	if linear_velocity <= Vector3.ZERO  and angular_velocity <= Vector3.ZERO:
+	var stop_signel_send_time : float = 1.0
+	if linear_velocity == Vector3.ZERO  and angular_velocity == Vector3.ZERO:
+		dice_moving = false
 		stopped.emit()
-
-func get_top_facing_side()->Side:
+	else:
+		dice_moving = true
+##Checks which side is closest to the given Vector3. Vector3.UP is default, which will give the side that faces up. and returns that side.
+func get_top_facing_side(comp_vector : Vector3 = Vector3.UP)->void:
+	var top_facing_side : Side = sides[0]
 	if sleeping:
 		for i in sides:
-			if i.global_transform.basis.z.normalized().dot(Vector3.UP) >= .9:
-				return i
-		return null
-	else:
-		printerr("Dice is not stopped yet.", self)
-		return null
+			if i.global_transform.basis.z.normalized().dot(comp_vector) >= top_facing_side.global_transform.basis.z.normalized().dot(comp_vector):
+				top_facing_side = i
+	current_side = top_facing_side
+	
--- dice-attack-game/turn_manager.gd
+++ dice-attack-game/turn_manager.gd
@@ -5,9 +5,21 @@ class_name Turn_Manager
 @export var dice_height : float = 4.0 ## The height at which the dice start when they are rolled.
 @export var random_impulse_strength_min : float = -1.5
 @export var random_impulse_strength_max : float = 1.5
+var can_check_if_roll_is_done : bool = true
+func _ready() -> void:
+	for i in dice_array:
+		if !i.ready:
+			await i.ready
+		i.freeze = true
+		i.visible = false
+		i.stopped.connect(check_if_roll_is_done)
 func roll_dice()->void:
+	can_check_if_roll_is_done = true
+	
 	for dice in dice_array:
 		var new_transform : Transform3D
+		dice.freeze = false
+		dice.visible = true
 		new_transform = dice.global_transform
 		new_transform.origin.y = dice_height
 		PhysicsServer3D.body_set_state(dice,PhysicsServer3D.BODY_STATE_TRANSFORM,new_transform)
@@ -22,3 +34,30 @@ func roll_dice()->void:
 			randf_range(random_impulse_strength_min, random_impulse_strength_max)
 			))
 	
+func check_if_roll_is_done()->void:
+		var roll_done : bool = true
+		for i in dice_array:
+			if i.dice_moving:
+				roll_done = false
+		
+		if roll_done and can_check_if_roll_is_done:
+			print(roll_done) 
+			can_check_if_roll_is_done = false
+			calculate_roll()
+		
+func calculate_roll()->void:
+	for i in dice_array:
+		i.get_top_facing_side()
+		i.freeze
+	move_dice_into_position()
+	
+func sort_dice(a:Dice, b:Dice)->bool:
+	if a.current_side.color < b.current_side.color :
+		return true
+	return false
+	
+func move_dice_into_position()->void:
+	print("-------NEW TABLE------------")
+	dice_array.sort_custom(sort_dice)	
+	for i in dice_array:
+		print(i.name , " ", i.current_side.color)
