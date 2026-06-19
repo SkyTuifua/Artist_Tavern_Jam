52b9fffdf9aafd6f127ae371ba007f5b00c33a40
Kevin Bacon <kevinbaconweb@gmail.com>
2026-06-19 16:36:23 -0600

    fix coin animation reset bug

--- dice-attack-game/coin_flip.gd
+++ dice-attack-game/coin_flip.gd
@@ -5,32 +5,41 @@ enum CoinResult {
 	TAILS
 }
 
+var start_position : Vector3
+var start_rotation : Vector3
 var current_coin_result : CoinResult
 signal coin_finished
 signal coin_result(result)
 
 @onready var animation_player: AnimationPlayer = $CoinAnimation
 
+func _ready():
+	start_position = position
+	start_rotation = rotation
 
 func start_coin_flow() -> void:
+	position = start_position
+	rotation_degrees = start_rotation
 	animation_player.play("coin_rise")
+	print("rise")
 	await animation_player.animation_finished
 	flip_coin()
 
 
 func flip_coin() -> void:
-
+	print("flip")
 	if randf() < 0.5:
 		current_coin_result = CoinResult.HEADS
 		animation_player.play("coin_heads")
 	else:
 		current_coin_result = CoinResult.TAILS
 		animation_player.play("coin_tails")
-		rotation_degrees.x = 180
 
 	# Wait until the flip animation is done
 	await animation_player.animation_finished
 	animation_player.play("coin_disappear")
-	rotation_degrees = Vector3.ZERO
+	await animation_player.animation_finished
+	animation_player.play("RESET")
+	await animation_player.animation_finished
 	coin_result.emit(current_coin_result)
 	coin_finished.emit()
